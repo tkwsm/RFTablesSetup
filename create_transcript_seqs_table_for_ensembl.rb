a5eb2e2eafd7da2b52ecbd496d512d008cf4556f
Tkwsm <kawashima38@gmail.com>
2022-02-13 21:32:31 +0900

    mod create_transcript_seqs_table_for_ensembl.rb

--- create_transcript_seqs_table_for_ensembl.rb
+++ create_transcript_seqs_table_for_ensembl.rb
@@ -9,6 +9,17 @@ ff = FlatFile.new( FastaFormat, ARGF )
 id = start_id
 ff.each do |f|
   id += 1
-  print [ project_id, f.definition, f.seq ].join("\t"), "\t", "#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\t#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\n"
+  transcript_id = f.definition
+  if   transcripit_id =~ /^transcript:\S/
+    transcript_id = "#{transcript_id.slice(/^(\S+)/, 1)}"
+  else
+    transcript_id = "transcript:#{transcript_id.slice(/^(\S+)/, 1)}"
+  end
+  if   transcripit_id =~ /^\S+\.\d+$/
+    transcript_id = "#{transcript_id.slice(/^(\S+)\.\d+$/, 1)}"
+  end
+
+  print [ project_id, transcript_id, f.seq ].join("\t"), "\t", "#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\t#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\n"
+
 end
 
