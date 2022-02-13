75dc24fec3cd2304acf2c7e9954790a5769d6273
Tkwsm <kawashima38@gmail.com>
2022-02-13 21:12:29 +0900

    add create_transcript_seqs_table_for_ensembl.rb

---
+++ create_transcript_seqs_table_for_ensembl.rb
@@ -0,0 +1,14 @@
+
+require 'bio'
+include Bio
+
+start_id = ( ARGV.shift ).to_i
+project_id = ARGV.shift
+ff = FlatFile.new( FastaFormat, ARGF )
+
+id = start_id
+ff.each do |f|
+  id += 1
+  print [ project_id, f.definition, f.sequence ].join("\t"), "\t", "#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\t#{Time.new.to_s.slice(/(.+)\s\S+$/, 1)}\n"
+end
+
